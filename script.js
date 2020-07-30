84d769685d0c212180f2b6bf86f676df9b0ef55c
Dan <daniel.murphy.1@gmail.com>
2020-07-29 20:13:16 -0400

    move 4 block snake across screen

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
 let canvas;
 let canvasContent;
-
+let snakeX = 25;
+let refreshInterval = 500;
 window.onload = function() {
     canvas = document.getElementById("game-canvas");
     canvasContent = canvas.getContext("2d");
+    // canvasContent.fillStyle = "black";
+    // canvasContent.fillRect(0,0,canvas.width,canvas.height);
+    // canvasContent.fillStyle = "red";
+    // canvasContent.fillRect(250,250,100,100);
+    // canvasContent.fillStyle = "white";
+    // canvasContent.fillRect(300,300,100,100);
+
+
+    
+    //setInterval(newBox, refreshInterval-100)
+    setInterval(moveSnake, refreshInterval);
+  
+   
+    
+}
+
+function moveSnake(){
+    snakeX = snakeX +5;
     canvasContent.fillStyle = "black";
     canvasContent.fillRect(0,0,canvas.width,canvas.height);
     canvasContent.fillStyle = "red";
     canvasContent.fillRect(250,250,100,100);
     canvasContent.fillStyle = "white";
     canvasContent.fillRect(300,300,100,100);
+    let box1 = {
+        color: canvasContent.fillStyle = "red",
+        postion: canvasContent.fillRect(snakeX,25,15,15),
+        
+        }
+        let box2 = {
+        color: canvasContent.fillStyle = "red",
+        position: canvasContent.fillRect(snakeX+17,25,15,15),
+       
+        }
+        let box3 = {
+            color: canvasContent.fillStyle = "red",
+        position: canvasContent.fillRect(snakeX+34,25,15,15),
+        
+        }
+        let box4 = {
+        color: canvasContent.fillStyle = "red",
+        position: canvasContent.fillRect(snakeX+51,25,15,15)
+        }
+    
+        let snake = [box1,box2,box3,box4];
+}
+
+function newBox(){
+   
+    canvasContent.fillStyle = "black";
+    canvasContent.fillRect(0,0,canvas.width,canvas.height);
     canvasContent.fillStyle = "red";
-    canvasContent.fillRect(25,25,15,15);
-    canvasContent.fillStyle = "red";
-    canvasContent.fillRect(42,25,15,15);
-    canvasContent.fillStyle = "red";
-    canvasContent.fillRect(59,25,15,15);
-    canvasContent.fillStyle = "red";
-    canvasContent.fillRect(76,25,15,15);
+    canvasContent.fillRect(250,250,100,100);
+    canvasContent.fillStyle = "white";
+    canvasContent.fillRect(300,300,100,100);
+    canvasContent.fillStyle = "blue";
+    canvasContent.fillRect(snakeX-17,50,15,15);
 }
