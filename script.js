75c66bc22cf1aef83f90270ae3a2c6f687cb3578
Dan <daniel.murphy.1@gmail.com>
2020-08-04 22:44:58 -0400

    added movement with arrow keys, generated apple

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 let canvas;
 let canvasContent;
-let snakeX = 25;
-let snakeXSpeed = 5;
+let snakeX = 20;
+let snakeY = 20;
+let snakeXSpeed = 20;
+let snakeYSpeed = 0;
+const rectSide = 20;
+let isApple = false;
+ let appleX = null;
+ let appleY = null;
+
 
 window.onload = function() {
     canvas = document.getElementById("game-canvas");
     canvasContext = canvas.getContext("2d");
-    const framesPerSecond = 30;
+    const framesPerSecond = 20;
     setInterval(function(){
 
     drawRect(0,0,canvas.width,canvas.height,"black");
-        newBox();
+    if (!isApple){
+        let appleX = generateAppleX();
+        let appleY = generateAppleY();
+       isApple = true;
+    }
+    
         moveSnake();
+        drawApple(appleX,appleY);
     }, 1000/framesPerSecond)   
 }
 
@@ -19,23 +32,79 @@ window.onload = function() {
 
 function moveSnake(){
     snakeX = snakeX + snakeXSpeed;
+    snakeY = snakeY + snakeYSpeed;
     if(snakeX >= canvas.width){
         snakeXSpeed = -snakeXSpeed;
     }
      if(snakeX <= 0){
          snakeXSpeed = -snakeXSpeed;
      }
-    let box1 = drawRect(snakeX,25,15,15,"red");
-    let box2 = drawRect(snakeX+17,25,15,15,"red");
-    let box3 = drawRect(snakeX+34,25,15,15,"red");
-    let box4 = drawRect(snakeX+51,25,15,15,"red");
-    let snake = [box1,box2,box3,box4];
+    let box1 = drawRect(snakeX,snakeY,rectSide, rectSide,"red");
+    
+
+
+    if (snakeX >= appleX && snakeX <= appleX + rectSide && snakeY >= appleY && snakeY <= appleY + rectSide){
+        canvasContext.clearRect(appleX, appleY, rectSide, rectSide);
+        isApple = false;
+        appleY = undefined;
+        appleX = undefined;
+    }
+
+    //move down
+    document.addEventListener("keydown", function(e){
+        if(e.which === 40){
+            snakeXSpeed = 0;
+            snakeYSpeed = 10;
+        }
+    })
+    
+    //move right
+    document.addEventListener("keydown", function(e){
+        if(e.which === 39){
+            snakeXSpeed = 10;
+            snakeYSpeed = 0;
+        }
+    })
+
+    //move up
+    document.addEventListener("keydown", function(e){
+        if(e.which === 38){
+            snakeXSpeed = 0;
+            snakeYSpeed = -10;
+        }
+    })
+
+    //move left
+    document.addEventListener("keydown", function(e){
+        if(e.which === 37){
+            snakeXSpeed = -10;
+            snakeYSpeed = 0;
+        }
+    })
 }
 
-function newBox(){
+
+function generateAppleX(){
+    const appleX = 20 * (Math.floor(Math.random()*30)); //generate random between 0 and 580
+    
+    return appleX;
    
-    drawRect(snakeX-17,50,15,15,"blue");
 }
+
+function generateAppleY(){
+    const appleY = 20 * (Math.floor(Math.random()*30)); //generate random between 0 and 580
+    return appleY;
+}
+
+// let appleX = generateAppleX();
+// let appleY = generateAppleY();
+
+function drawApple(appleX, appleY){
+    
+    drawRect(appleX, appleY, rectSide, rectSide, "green");
+   
+}
+
 
 function drawRect(leftX,topY,width,height,color){
     canvasContext.fillStyle = color;
