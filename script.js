70f1fe4baf89c4226ad74627fbc8d0859c6e107a
Dan <daniel.murphy.1@gmail.com>
2020-08-08 23:53:26 -0400

    movement in a chain, snake grows when eats apple,finished border detection, game over screen

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-
+const DEBUG = false;
 
 let canvas;
 let canvasContent;
 let snakeX = 20;
 let snakeY = 20;
-let snakeXSpeed = 20;
-let snakeYSpeed = 0;
+let snakeHeadXSpeed = 20;
+let snakeHeadYSpeed = 0;
 const rectSide = 20;
 const gridSize = 20;
 let isApple = false;
 let appleX = null;
 let appleY = null;
 let snakeBody = [
-    {x: 60, y: 20, xSpeed: snakeXSpeed, ySpeed: snakeYSpeed},
-    {x: 38, y: 20, xSpeed: snakeXSpeed, ySpeed: snakeYSpeed}, 
-    {x: 16, y: 20, xSpeed: snakeXSpeed, ySpeed: snakeYSpeed},
+    {x: 60, y: 20, xSpeed: snakeHeadXSpeed, ySpeed: snakeHeadYSpeed},
+    {x: 40, y: 20, xSpeed: snakeHeadXSpeed, ySpeed: snakeHeadYSpeed}, 
+    {x: 20, y: 20, xSpeed: snakeHeadXSpeed, ySpeed: snakeHeadYSpeed},
 ];
+let gameOver = false;
 let counter = 0;
+let direction = "right";
 
-
+//main game loop
 window.onload = function() {
     canvas = document.getElementById("game-canvas");
     canvasContext = canvas.getContext("2d");
     const framesPerSecond = 10;
+
     setInterval(function(){
 
     drawRect(0,0,canvas.width,canvas.height,"black");
+    if(gameOver){
+        canvasContext.fillStyle = "red";
+        canvasContext.font = "30px Arial";
+        canvasContext.fillText("Game Over", 100, 100);
+        canvasContext.fillText("Reload Window to Play Again", 100, 200);
+        return;
+    }
     if (!isApple){
         appleX = generateAppleX();
         appleY = generateAppleY();
@@ -39,91 +49,168 @@ window.onload = function() {
 }
 
 
-
 function moveSnake(){
-    snakeBody[0].x += snakeBody[0].xSpeed;
-    snakeBody[0].y += snakeBody[0].ySpeed;
-    snakeBody[1].x += snakeBody[1].xSpeed;
-    snakeBody[1].y += snakeBody[1].ySpeed;
-    snakeBody[2].x += snakeBody[2].xSpeed;
-    snakeBody[2].y += snakeBody[2].ySpeed;
-    if(snakeBody[0].x >= canvas.width){
-        snakeXSpeed = -snakeXSpeed;
-    }
-    if(snakeBody[0].x <= 0){
-        snakeXSpeed = -snakeXSpeed;
+if(gameOver){
+    return;
+}
+
+    //update positon of all body pieces other than head
+    for (let i = snakeBody.length -1; i > -1; i--){
+        if(i !== 0){
+            snakeBody[i].x = snakeBody[i - 1].x;
+            snakeBody[i].y = snakeBody[i - 1].y;
+        }else {
+            //update position of head
+            snakeBody[i].x += snakeHeadXSpeed;
+            snakeBody[i].y += snakeHeadYSpeed;
+        }
     }
     
+    if(snakeBody[0].x > canvas.width){
+        console.log("game over");
+        gameOver = true;
+    }
+    if(snakeBody[0].x < 0){
+        console.log("game over");
+        gameOver = true;
+    }
+    if(snakeBody[0].y > canvas.height){
+        console.log("game over");
+        gameOver = true;
+    }
+    if(snakeBody[0].y < 0){
+        console.log("game Over");
+        gameOver = true;
+    }
+    
+    for (let i = 1; i <= snakeBody.length-1; i++){
+        if (snakeBody[0].x === snakeBody[i].x && snakeBody[0].y === snakeBody[i].y){
+            //alert("game over");
+            gameOver = true;
+        }
+    }
 
-    //eat apple, increase counter/score
+    //eat apple, increase counter/score, add piece to body
     if (snakeBody[0].x === appleX && snakeBody[0].y === appleY){
         canvasContext.clearRect(appleX, appleY, rectSide, rectSide);
         isApple = false;
         appleY = undefined;
         appleX = undefined;
         counter++;
-        drawSnake();
+        createBodyPiece();
+        //drawSnake();
     }
 
-    //move down
+    changeDirection();
+    // //move down
+    // document.addEventListener("keydown", function(e){
+    //     if(e.which === 40){
+            
+    //         snakeHeadXSpeed = 0;
+           
+    //         snakeHeadYSpeed = gridSize;
+    //     }
+    // })
+    
+    // //move right
+    // document.addEventListener("keydown", function(e){
+    //     if(e.which === 39){
+    //         snakeHeadXSpeed = gridSize;
+    //         snakeHeadYSpeed = 0;
+    //     }
+    // })
+
+    // //move up
+    // document.addEventListener("keydown", function(e){
+    //     if(e.which === 38){
+    //         snakeHeadXSpeed = 0;
+    //         snakeHeadYSpeed = -gridSize;
+    //     }
+    // })
+
+    // //move left
+    // document.addEventListener("keydown", function(e){
+    //     if(e.which === 37){
+    //         snakeHeadXSpeed = -gridSize;
+    //         snakeHeadYSpeed = 0;
+    //     }
+    // })
+    
+}
+
+function changeDirection(){
     document.addEventListener("keydown", function(e){
+        //move down
         if(e.which === 40){
-            //snakeXSpeed = 0;
-
-            for(i = 0; i <= snakeBody.length-1; i++){
-                snakeBody[i].xSpeed = 0;
-                snakeBody[i].ySpeed = gridSize;
-            }
-            //snakeXSpeed = 0;
-            snakeYSpeed = gridSize;
+            if(direction === "up"){
+                return;
+            };
+            snakeHeadXSpeed = 0;
+            snakeHeadYSpeed = gridSize;
+            direction = "down";
         }
-    })
-    
-    //move right
-    document.addEventListener("keydown", function(e){
+        //move right
         if(e.which === 39){
-            snakeXSpeed = gridSize;
-            snakeYSpeed = 0;
+            if(direction === "left"){
+                return;
+            };
+            snakeHeadXSpeed = gridSize;
+            snakeHeadYSpeed = 0;
+            direction = "right";
         }
-    })
-
-    //move up
-    document.addEventListener("keydown", function(e){
+        //move up
         if(e.which === 38){
-            snakeXSpeed = 0;
-            snakeYSpeed = -gridSize;
+            if(direction === "down"){
+                return;
+            };
+            snakeHeadXSpeed = 0;
+            snakeHeadYSpeed = -gridSize;
+            direction = "up";
+        }
+        //move left
+        if(e.which === 37){
+            if(direction === "right"){
+                return;
+            };
+            snakeHeadXSpeed = -gridSize;
+            snakeHeadYSpeed = 0;
+            direction = "left";
         }
     })
 
-    //move left
-    document.addEventListener("keydown", function(e){
-        if(e.which === 37){
-            snakeXSpeed = -gridSize;
-            snakeYSpeed = 0;
-        }
-    })
-    
+}
+
+function createBodyPiece(){
+    //create new body part on top of last body part - will update to end of snake on new draw
+    snakeBody.push({x: (snakeBody[snakeBody.length-1].x), y: snakeBody[snakeBody.length-1].y});
 }
 
 
 function generateAppleX(){
-    const appleX = 20 * (Math.floor(Math.random()*30)); //generate random between 0 and 580
+     appleX = 20 * (Math.floor(Math.random()*30)); //generate random between 0 and 580
     
     return appleX;
 }
 
 function generateAppleY(){
-    const appleY = 20 * (Math.floor(Math.random()*30)); //generate random between 0 and 580
+
+     appleY = 20 * (Math.floor(Math.random()*30)); //generate random between 0 and 580
+    
     return appleY;
 }
 
-
 function drawApple(appleX, appleY){
-    
+    // if(snakeBody.some(coordinate => coordinate.x = appleX) && snakeBody.some(coordinate => coordinate.y = appleY)){
+    //     generateAppleY();
+    //     generateAppleY();
+    //     drawApple();
+    // }else {
     drawRect(appleX, appleY, rectSide, rectSide, "green");
-}
+    }
+//}
 
 
+//draw rectangle helper function
 function drawRect(leftX,topY,width,height,color){
     canvasContext.fillStyle = color;
     canvasContext.fillRect(leftX,topY,width,height);
@@ -134,19 +221,19 @@ function drawSnake(){
     let spaceBetweenRects = 2;
     
     // for(i = 0; i <= counter; i++){
-    //     if(snakeXSpeed >0){
+    //     if(snakeHeadXSpeed >0){
     //        drawRect(snakeBody[0].x-((rectSide * i) + (spaceBetweenRects * i)), snakeBody[0].y, rectSide, rectSide, "red");            
-    //     } else if(snakeXSpeed < 0){
+    //     } else if(snakeHeadXSpeed < 0){
     //         drawRect(snakeBody[0].x+((rectSide * i) + (spaceBetweenRects * i)), snakeBody[0].y, rectSide, rectSide, "red");
-    //     }else if(snakeYSpeed > 0){
+    //     }else if(snakeHeadYSpeed > 0){
     //          drawRect(snakeBody[0].x, snakeBody[0].y-((rectSide * i) + (spaceBetweenRects * i)), rectSide, rectSide, "red");
-    //     }else if(snakeYSpeed < 0){
+    //     }else if(snakeHeadYSpeed < 0){
     //          drawRect(snakeBody[0].x, snakeBody[0].y+((rectSide * i) + (spaceBetweenRects * i)), rectSide, rectSide, "red");
     //     }
     // }
-    drawRect(snakeBody[0].x, snakeBody[0].y, rectSide, rectSide, "red");
-    drawRect(snakeBody[1].x, snakeBody[1].y, rectSide, rectSide, "red");
-    drawRect(snakeBody[2].x, snakeBody[2].y, rectSide, rectSide, "red");
+    for(let segment of snakeBody) {
+        drawRect(segment.x, segment.y, rectSide, rectSide, "red");
+    }
 
 };
 
